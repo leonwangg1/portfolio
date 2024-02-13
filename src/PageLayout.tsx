baa6db36a86e0579b38976939fc45f80bc42c723
Leon <itsleonwang1@gmail.com>
2024-02-14 00:19:51 +1100

    Remove Analytics component from App.tsx and add Analytics component to PageLayout.tsx

--- src/PageLayout.tsx
+++ src/PageLayout.tsx
@@ -2,10 +2,12 @@ import React from "react";
 
 import { Navbar } from "./components/Navbar";
 import Footer from "./components/Footer";
+import { Analytics } from "@vercel/analytics/react";
 
 const PageLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="py-8 px-10 lg:px-64">
+      <Analytics />
       <header className="mb-60 z-50">
         <Navbar />
       </header>
