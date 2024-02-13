7da027f737f11feac0be9e6b2686830302e6d23a
Leon <itsleonwang1@gmail.com>
2024-02-14 00:16:35 +1100

    Add analytics component to App.tsx and remove analytics component from main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <BrowserRouter>
+      <Analytics />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
