24cbe3a5e3dc2694abbdb0d5e884a43f734d7e5d
Leon <itsleonwang1@gmail.com>
2024-02-19 04:21:05 +1100

    Update LayoutGrid component

--- src/components/LayoutGrid.tsx
+++ src/components/LayoutGrid.tsx
@@ -3,7 +3,8 @@
 import { cn } from "../utils/cn";
 import "../App.css";
 
-const descStyle = "text-white lg:text-lg";
+const descStyle =
+  "text-neutral-500 text-sm mt-1 lg:text-[1rem] capitalize body-font";
 
 export const LayoutGrid = () => {
   const cards = [
@@ -13,9 +14,7 @@ export const LayoutGrid = () => {
       thumbnail: "/surveyapp.png",
       desc: (
         <a>
-          <p className={descStyle}>
-            <strong>LEA Career Preference Tool</strong>
-          </p>
+          <p className={descStyle}>LEA Career Preference Tool</p>
         </a>
       ),
       fulldesc: (
@@ -26,18 +25,13 @@ export const LayoutGrid = () => {
           pathways.
         </p>
       ),
+      link: "",
     },
     {
       id: 2,
       className: "cardscroll col-span-1 md:col-span-3",
       thumbnail: "/wang.png",
-      desc: (
-        <a href="https://www.wangs-therapy.com" target="_blank">
-          <p className={descStyle}>
-            <strong>Wang's Natural Therapy Centre</strong>
-          </p>
-        </a>
-      ),
+      desc: <p className={descStyle}>Wang's Natural Therapy Centre</p>,
       fulldesc: (
         <p>
           <strong>Wang's Natural Therapy Centre.</strong> Web design and
@@ -46,18 +40,13 @@ export const LayoutGrid = () => {
           acupuncture and holistic services.
         </p>
       ),
+      link: "https://www.wangs-therapy.com",
     },
     {
       id: 3,
-      className: "cardscroll col-span-1 md:col-span-3",
+      className: "cardscroll col-span-1 md:col-span-4",
       thumbnail: "/mimiblooms.png",
-      desc: (
-        <a href="https://www.mimiblooms.store" target="_blank">
-          <p className={descStyle}>
-            <strong>MimiBlooms Ecommerce Platform</strong>
-          </p>
-        </a>
-      ),
+      desc: <p className={descStyle}>MimiBlooms Ecommerce Platform</p>,
       fulldesc: (
         <p>
           <strong>MimiBlooms.</strong> An ecommerce platform which features a
@@ -66,41 +55,40 @@ export const LayoutGrid = () => {
           Stripe for secure payment processing.
         </p>
       ),
+      link: "https://mimiblooms.store/au",
     },
     {
       id: 4,
-      className: "cardscroll col-span-1 md:col-span-3",
-      thumbnail:
-        "https://images.unsplash.com/photo-1475070929565-c985b496cb9f?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      className: "cardscroll col-span-1 md:col-span-2",
+      thumbnail: "/mca.png",
       desc: "",
+      link: "",
     },
   ];
 
   return (
-    <div className="w-full h-full py-5 grid grid-cols-1 md:grid-cols-6 mx-auto gap-5">
+    <div className="w-full h-full py-5 grid grid-cols-1 md:grid-cols-6 mx-auto gap-2">
       {cards.map((card, i) => (
         <div
-          key={i}
           className={cn(
             card.className,
-            "bg-neutral-800 rounded-xl w-full h-full relative cardanimate"
+            "bg-[#19191a] rounded-xl w-full h-full relative cardanimate"
           )}
         >
-          <div className="flex flex-col items-center text-center w-fit h-full relative">
-            <img src={card.thumbnail} className="rounded-xl" />
-
+          <div>
             <div
-              className="absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center p-2 md:p-4 rounded-xl"
-              style={
-                {
-                  // background:
-                  //   "linear-gradient(to top, rgba(0,0,0,0.55) 0%, #0000 30%)",
-                }
-              }
+              key={i}
+              className="flex flex-col items-top text-center w-fit h-full relative"
             >
-              <span className=" text-pretty text-md md:text-[0.8rem] z-10 cursor-pointer uppercase">
-                {card.desc}
-              </span>
+              <img
+                src={card.thumbnail}
+                className="scale-[.90] p-5 cursor-pointer"
+                onClick={() => {
+                  if (typeof card.link === "string") {
+                    window.location.href = card.link;
+                  }
+                }}
+              />
             </div>
           </div>
         </div>
